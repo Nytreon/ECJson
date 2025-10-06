b3af8502e9deb7f925970dc5ab934e2c90b0aa29
Nytreon <devcytroy@gmail.com>
2025-10-06 02:47:54 +0000

    Update exampleScript.js

--- exampleScript.js
+++ exampleScript.js
@@ -1,4 +1,14 @@
+//import the function
 import returnEntityJson from "./entities/returnEntityJson.js";
 
+//request the pig's json
+const pigJson = returnEntityJson("minecraft:pig")
 
-console.log(JSON.stringify(returnEntityJson("minecraft:pig")))
+//console log the entire json
+console.log(JSON.stringify(pigJson))
+
+//grab the collision box from the pig's json
+const collision_box = pigJson.["minecraft:entity"].components?.["minecraft:collision_box"];
+
+//console log collision box dimensions
+console.log(JSON.stringify(collision_box))
